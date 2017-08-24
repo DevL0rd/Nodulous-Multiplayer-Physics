7f0b96899a7834d5784c4bcdd670cd24a6e5f8e3
dmhzmxn <dmhzmxn@gmail.com>
2017-08-23 23:35:46 -0600

    Cleaned up.

--- Devlord_modules/DB.js
+++ Devlord_modules/DB.js
@@ -4,14 +4,13 @@
 //Version: 0.1.6
 var fs = require('fs');
 var dirpath = "./"
-
 function load(str) {
-	var contents = fs.readFileSync(str).toString();
+	var contents = fs.readFileSync(str).toString('utf-8');
 	return JSON.parse(contents)
 }
 function save(str, obj) {
 	var contents = JSON.stringify(obj, null, "\t")
-	fs.writeFile(dirpath + str + ".json", contents, function(err) {
+	fs.writeFile(str, contents, function(err) {
 		if(err) throw err;
 	}); 
 }
--- InstallModules.bat
+++ InstallModules.bat
@@ -1,7 +1,5 @@
 cd Engine
 start InstallModules.bat
-cd ../OpenStats
-start InstallModules.bat
 cd  ..
 npm install --save
 exit
